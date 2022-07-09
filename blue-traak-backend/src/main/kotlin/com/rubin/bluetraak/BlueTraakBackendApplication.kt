edefac6400fc67ac63023c3b1217bf876e40b8b1
Nikita Bundin <nbundin@solanteq.com>
2022-07-09 13:09:05 +0300

    Initial commit

---
+++ blue-traak-backend/src/main/kotlin/com/rubin/bluetraak/BlueTraakBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.rubin.bluetraak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlueTraakBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlueTraakBackendApplication>(*args)
+}
